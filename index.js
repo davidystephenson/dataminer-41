d172574f2d4cbee6046e3def674b13f3bb419a53
David Y. Stephenson <david@davidystephenson.com>
2020-06-05 17:32:17 +0200

    Define endpoints

--- index.js
+++ index.js
@@ -4,6 +4,44 @@ const app = express()
 
 const port = 3000
 
+const users = [
+  {
+    name: 'david',
+    hours: 10,
+    website: 'cnn.com'
+  },
+  {
+    name: 'jeroen',
+    hours: 4,
+    website: 'nos.nl'
+  },
+  {
+    name: 'rein',
+    hours: 6,
+    website: 'codaisseur.com'
+  }
+]
+
+app.get(
+  '/users',
+  (request, response) => {
+    response.send(users)
+  }
+)
+
+app.get(
+  '/user/:username',
+  (request, response) => {
+    console.log(request.params.username)
+
+    const found = users.find(
+      (user) => user.name === request.params.username
+    )
+
+    response.send(found)
+  }
+)
+
 app.listen(
   port,
   () => console.log(`Listening on :${port}`)
